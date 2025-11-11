679a2271a76f16241434ae291b80817c67b6e750
peteywee <patricktssllc@gmail.com>
2025-11-11 01:50:45 -0600

    refactor: remove fresh-schedules-types.d.ts shim - use packages/types directly

--- apps/web/src/types/fresh-schedules-types.d.ts
+++
@@ -1,8 +0,0 @@
-// [P2][APP][CODE] Fresh Schedules Types D type definitions
-// Tags: P2, APP, CODE
-// Minimal local module mapping to the workspace types package.
-// This file intentionally re-exports the sources from `packages/types/src`
-// so that imports of "@fresh-schedules/types" resolve during local dev.
-declare module "@fresh-schedules/types" {
-  export * from "../../../../packages/types/src";
-}
